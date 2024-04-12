e6992a165a284d1789ef3ac3cda7fa3b755b9c36
JUNILMIN <jimin@aim.co.kr>
2024-04-12 09:59:31 +0900

    week01

---
+++ src/main/java/com/example/JavaPractice/week01/JavaBasic.java
@@ -0,0 +1,8 @@
+package com.example.JavaPractice.week01;
+
+// JDK
+// 1. compiler: .java -> .class
+// JRE 역할
+// JDB : 디버깅
+public class JavaBasic {
+}
--- src/main/java/com/example/JavaPractice/week01/TEST.java
+++
@@ -1,7 +0,0 @@
-package com.example.JavaPractice.week01;
-
-public class TEST {
-    public static void main(String[] args) {
-        System.out.println("한글");
-    }
-}
