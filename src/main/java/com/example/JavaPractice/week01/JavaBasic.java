de9b661d6632cafc96520e5ee3b52278edcd5461
JUNILMIN <jimin@aim.co.kr>
2024-04-12 10:17:33 +0900

    week01

--- src/main/java/com/example/JavaPractice/week01/JavaBasic.java
+++ src/main/java/com/example/JavaPractice/week01/JavaBasic.java
@@ -5,4 +5,16 @@ package com.example.JavaPractice.week01;
 // JRE 역할
 // JDB : 디버깅
 public class JavaBasic {
+
+    // 객체: 특징(속성, 변수), 행동: 메서드
+    // 온점: 하위 요소 표현
+
+    public void TestMethod(){
+        System.out.println("test2");
+    }
+    public static void main(String[] args) {
+        System.out.println("test");
+        JavaBasic jb = new JavaBasic();
+        jb.TestMethod();
+    }
 }
---
+++ src/main/java/com/example/JavaPractice/week01/Variable.java
@@ -0,0 +1,21 @@
+package com.example.JavaPractice.week01;
+
+
+// 변수: 하나의 값을 저장할 수 있는 저장 공간
+// 기본형 변수: 논리형 변수, 문자형 변수, 정수형 변수, 실수형 변수
+// 참조형 변수: 문자열 변수, Object, Array 등..
+// 래퍼 클래스 변수: 기본형 변수를 클래스로 한번 랩핑(감싸는) 변수
+    //byte  Byte
+    //short  Short
+    //int Integer
+    //long Long
+    //float  Float
+    //double  Double
+    //char  Character
+    //boolean  Boolean
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+    }
+}
