c6fb36438a4dc7b7f697224180b546f327da9aa0
JUNILMIN <jimin@aim.co.kr>
2024-04-15 14:43:08 +0900

    week02

---
+++ src/main/java/com/example/JavaPractice/week02/LoopPractice.java
@@ -0,0 +1,14 @@
+package com.example.JavaPractice.week02;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+        // 구구단 만들기
+        for(int i = 1; i < 10; i++){
+            System.out.println(i + "단 시작");
+            for(int j = 1; j < 10; j++){
+                System.out.println(i*j);
+            }
+            System.out.println(i + "단 종료");
+        }
+    }
+}
--- src/main/java/com/example/JavaPractice/week02/WhilePractice.java
+++ src/main/java/com/example/JavaPractice/week02/WhilePractice.java
@@ -16,9 +16,19 @@ public class WhilePractice {
         while (number < 3) {
             number++;
             if (number == 2) {
-                break;
+                continue;
             }
             System.out.println(number);
+            // 처음 반복문 -> 0 은 3보다 작으므로
+            // 0 ++ = 1 이 된다.
+            // 1 은 2 가 아니므로 1 출력
+            // 1 은 3 보다 작으므로
+            // 1++ = 2 가 된다.
+            // 2 == 2 이므로 하위 생략하고 반복문 처음으로 간다.
+            // 2 는 3 보다 작으므로
+            // 2++ = 3 이 된다.
+            // 3 은 2 가 아니므로 3 출력
+            // 3 < 3 는 false 이므로 반복문 종료
         }
     }
 }
