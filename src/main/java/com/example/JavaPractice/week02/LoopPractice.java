1524e7729884e6a527443fe5b80dd1676b358da2
JUNILMIN <jimin@aim.co.kr>
2024-04-15 14:46:09 +0900

    week02

--- src/main/java/com/example/JavaPractice/week02/LoopPractice.java
+++ src/main/java/com/example/JavaPractice/week02/LoopPractice.java
@@ -6,7 +6,7 @@ public class LoopPractice {
         for(int i = 1; i < 10; i++){
             System.out.println(i + "단 시작");
             for(int j = 1; j < 10; j++){
-                System.out.println(i*j);
+                System.out.println(i +" x "+ j + " = " + i*j);
             }
             System.out.println(i + "단 종료");
         }
